e55e473358bd0f9b9dbb6c9c05f22f5378ec7f91
Hoang Anh <anhnh265@gmail.com>
2024-11-18 00:29:43 +0700

    add file operato-use.htnl

---
+++ session-07-varlable_and_data_type/JS/operator-use.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Nhập width");
+inputHeight = prompt("Nhập height");
+
+let width =parseInt(inputWidth);
+let height =parseInt(inputHeight);
+let area = width*height;
+document.write("The area is " + area);
